9774a8d884aea537e7aaf7b4e5c1251e311245c7
Malika <abdikarimovam88@gmail.com>
2023-11-13 21:32:15 +0500

    dsdsd

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -4,7 +4,7 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     bool test = true; 
-    string answer;  
+    std::string answer;  
     for(int i = 0; i <= 16; i++)
     {
         if(macAddress[i] != 'F' && macAddress[i] != ':')
@@ -14,7 +14,7 @@ std::string problemSolution4(const std::string &macAddress) {
         } 
     }
     int number; 
-    map <char, int> m; 
+    std::map <char, int> m; 
     m['0'] = 0; m['1'] = 1; m['2'] = 2; m['3'] = 3; m['4'] = 4; m['5'] = 5;
     m['6'] = 6; m['7'] = 7; m['8'] = 8; m['9'] = 9; m['A'] = 10; m['B'] = 11; 
     m['C'] = 12; m['D'] = 13; m['E'] = 14; m['F'] = 15; 
