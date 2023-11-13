6b3e67bfb06d8e0b85fab24e848577ed468df91f
Malika <abdikarimovam88@gmail.com>
2023-11-13 19:43:38 +0500

    finall

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,19 +2,19 @@
 
 std::string problemSolution3(float height, char S) {
     string result;
-    if (S = 'F') {
-        if (height < 160){
-            result = "Short";
-        } else if (height >= 160 || height < 175) {
-            result = "Normal";
-        } else if (height >= 175) 
-            result = "Tall";
-    } else {
+    if (S = 'M') {
         if (height < 170){
             result = "Short";
         } else if (height >= 170 || height < 185) {
             result = "Normal";
         } else if (height >= 185) 
+            result = "Tall";
+    } else if (S = 'F') {
+        if (height < 160){
+            result = "Short";
+        } else if (height >= 160 || height < 175) {
+            result = "Normal";
+        } else if (height >= 175) 
             result = "Tall";
     }
     return result;
