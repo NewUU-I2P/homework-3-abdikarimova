758bf64aafe1dea2d2068738ce7f0e0e005fa69a
Malika <abdikarimovam88@gmail.com>
2023-11-13 21:44:24 +0500

    pdfgofdpjgfd

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -6,8 +6,8 @@ float problemSolution1(float consumed_water) {
             cost = 13 + 30*0.4 + 20*0.12 + (consumed_water-50)*1.4;
         } else if (consumed_water > 30) {
             cost = 13 + 30*0.4 + (consumed_water-30) * 0.12;
-        } else 
+        } else {
             cost = 13 + consumed_water*0.5;
-
+        }
         return cost;
 } 
